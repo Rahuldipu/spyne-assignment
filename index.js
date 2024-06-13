5a871f54cb82ca0d8064e63df447a10736f77204
Rahuldipu <rk617605@gmail.com>
2024-06-13 22:53:17 +0530

    Update PORT from env

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ app.use("/v1/user", userRouter);
 app.use("/v1/post", postRouter);
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log(`Server is running on port 3000`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
